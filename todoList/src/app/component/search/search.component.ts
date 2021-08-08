c5366fb15b8b0dd5dd701dc95dfb93fd0a660b99
yangjingqi <yangjingqi@kungeek.com>
2021-08-08 18:11:35 +0800

    fix: 测试viewChild和生命周期after

--- todoList/src/app/component/search/search.component.ts
+++ todoList/src/app/component/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    //获取不到dom节点
+
+    let box: any = document.getElementById('box');
+    console.log(box.innerHTML)
+    box.style.color = '#008c8c';
+    box.style.height = 200
+    box.style.weight = 200
+  }
+
+  //视图加载完成以后触发的方法 dom加载完成  建议把dom元素都放在这个里面
+  ngAfterViewInit(): void {
+    let box: any = document.getElementById('box');
+
+    box.style.color = '#008c8c';
+    box.style.height = '200px';
+    box.style.weight = '200px';
+    console.log(box.innerHTML)
   }
 
 }
--- todoList/src/app/services/storage.service.ts
+++ todoList/src/app/services/storage.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  get() {
+    console.log('调用了get方法')
+  }
 }
