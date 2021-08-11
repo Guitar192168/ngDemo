19be16c3cff8638b09a1f996b77f2046b08c6b44
yangjingqi <yangjingqi@kungeek.com>
2021-08-11 14:09:18 +0800

    fix: 使用rxjs的map和filter需要导入rxjs/operators模块

--- requestAngular05/src/app/components/home/home.component.ts
+++ requestAngular05/src/app/components/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { RequestService } from 'src/app/services/request.service';
 
+// 要使用filter和map需要导入rxjs/operators
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -14,17 +17,60 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
+    // 1.同步获取
     let data = this.request.getData();
     console.log(data);
 
 
 
-    // 回调函数
-    this.request.getCallBackData((data:any) => {
+    //2. 回调函数
+    this.request.getCallBackData((data: any) => {
       console.log(data);
     });
 
-  
+    // 3. rxjs
+    let rxjsData = this.request.getRxjsData();
+
+    rxjsData.subscribe(data => {
+      console.log(data);
+    })
+
+
+    // 4.可以取消请求的rxjs
+    let unSubscribeData = this.request.getUnsubscribeRxjs();
+
+    let un = unSubscribeData.subscribe(data => {
+      console.log(data);
+    })
+
+    setTimeout(() => {
+      un.unsubscribe();
+    }, 1000)
+
+
+    // 5.不停发请求的rxjs
+    let SetIntervalRxjs = this.request.getSetIntervalRxjs();
+
+    // SetIntervalRxjs.subscribe((data:any) => {
+    //   console.log(data);
+    // });
+
+
+
+
+
+    // 6.rxjs的map和filter
+    let getMapRxjs = this.request.getMapRxjs();
+
+    // getMapRxjs.pipe(
+    //   filter((val:any)=>{
+    //     return val % 2 === 0
+    //   })
+    // )
+
+    // .subscribe(data => {
+    //   console.log(data,'来自rxjs的data');
+    // })
 
   }
 
--- requestAngular05/src/app/services/request.service.ts
+++ requestAngular05/src/app/services/request.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 
 @Injectable({
@@ -33,6 +34,44 @@ export class RequestService {
         var username = 'rxjs的name';
         observer.next(username);
       }, 2000)
+    })
+  }
+
+
+  // todo
+  // rxjs可以主动取消请求 （其实请求是发送了的，只是不接收
+
+  getUnsubscribeRxjs() {
+    return new Observable(observer => {
+      setTimeout(() => {
+        let name = '来自可以取消请求的rxjs';
+
+        observer.next(name);
+      }, 3000)
+    })
+  }
+
+  // rxjs连续发送请求
+  getSetIntervalRxjs() {
+    let count = 0;
+    return new Observable(observer => {
+      setInterval(() => {
+        count++;
+        let name = "不间断的发请求的rxjs" + count;
+        observer.next(name);
+      }, 1000)
+    })
+  }
+
+
+  // 使用一下map和filter
+  getMapRxjs() {
+    return new Observable(observer => {
+      let count: number = 0;
+      setInterval(() => {
+        count++;
+        observer.next(count)
+      }, 1000)
     })
   }
 }
