ef3b187bdff87df75309e509675c370bb83fcab8
yangjingqi <yangjingqi@kungeek.com>
2021-10-10 20:03:52 +0800

    fix: 给路由补充通过js控制get传值，需要新引入模块

--- router08/src/app/components/home/home.component.ts
+++ router08/src/app/components/home/home.component.ts
@@ -6,7 +6,9 @@
  * @Description: In User Settings Edit
  * @FilePath: /ngDemo-master/router08/src/app/components/home/home.component.ts
  */
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Router, NavigationExtras} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -15,11 +17,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public list: any = [];
-  constructor() {}
+
+  constructor(public router: Router) {
+  }
 
   ngOnInit(): void {
     for (let i = 0; i <= 9; i++) {
       this.list.push('这是第' + i + '个数据');
     }
   }
+
+  goToPage() {
+    this.router.navigate(['/detail/', '1234'])
+    // this.router.navigate(['/detail/'])
+  }
+
+
+  goGetPage() {
+    let queryParams: NavigationExtras = {
+      queryParams: {
+        uuuuuid: 1
+      }
+    }
+
+    // 无法跳转到动态路由的页面
+    this.router.navigate(['/product'], queryParams)
+  }
+
+
 }
